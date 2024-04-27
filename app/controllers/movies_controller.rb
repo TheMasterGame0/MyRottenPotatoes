d6c1174898b5ebd212b5e2206f53146c00c061bc
Gabriel Henrique <gabrielneres@ibict.br>
2024-04-26 21:58:55 -0300

    :hammer: Removendo cometários

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -45,13 +45,4 @@ class MoviesController < ApplicationController
     def movie_params
       params.require(:movie).permit(:titulo, :classificacao, :descricao, :dataLancamento)
     end
-
-  # def create
-  #   if (@movie = Movie.create(params.require(:movie).permit(:titulo, :classificacao, :descricao, :dataLancamento)))
-  #     redirect_to movies_path, :notice => "#{movie.title} criado."
-  #   else
-  #     flash[:alert] = "Movie #{movie.title} não pode ser criado: " + @movie.errors.full_messages.join(",")
-  #     render 'new'
-  #   end
-  # end
 end
